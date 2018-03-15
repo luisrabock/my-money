09776e75467ef8ea6f45be724206359fd1260831
Luis Rabock <luis.rabock@hotmail.com>
2018-03-15 01:35:31 -0300

    Finishing backend of myMoneyApp

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+module.exports = mongoose.connect('mongodb://localhost/mymoney', {
+ useMongoClient: true
+});
 
-mongoose.Promise = global.Promise
 
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.Number.min = 
+    "O  '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+
+mongoose.Error.messages.Number.max = 
+    "O '{VALUE} informado é  maior do que o limite máximo de '{MAX}'."
+
+mongoose.Error.messages.String.enum =
+    "O '{VALUE} informado não é valido para o atributo '{PATH}'."
--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,13 +1,16 @@
 const port = 3003
 
 const bodyParser = require('body-parser')
-
 const express = require('express')
+const queryParser = require('express-query-int')
+const AllowCors = require('./cors')
 
 const server = express()
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(AllowCors)
+server.use(queryParser())    
 
 server.listen(port, function() {
     console.log(`Servidor rodando na porta: ${port}`)
