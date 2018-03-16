647bb41150529dd30bd59c1434dd796364d5fc9e
Luis Rabock <luis.rabock@hotmail.com>
2018-03-16 05:37:04 -0300

    Start frontend with react and config webpack

---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../common/template/dependencias'
+import Header from '../common/template/header'
+
+
+export default (props) => (
+    <div className="wrapper">
+       <Header />
+    </div>
+)
