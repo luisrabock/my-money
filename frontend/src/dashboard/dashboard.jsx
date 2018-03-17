17d66f4a0a2d2d15829c6af37c2ccdf524a1097e
Luis Rabock <luis.rabock@hotmail.com>
2018-03-17 18:59:41 -0300

    Finishing dashboard and build dashboard2 without redux

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,4 +1,8 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+
+import {getSummary} from './dashboardActions'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 import ValueBox from '../common/widget/valueBox'
@@ -6,23 +10,30 @@ import Row from '../common/layout/row'
 
 
 class Dashboard extends Component {
+
+    componentWillMount() {
+        this.props.getSummary()
+    }
+
     render() {
+    const {credit, debt} = this.props.summary
         return (
                 <div>
                     <ContentHeader title='Dashboard' small='Versao 1.0' />
                      <Content>
                         <Row>
                             <ValueBox cols='12 4' color='green' icon='bank'
-                                    value='R$ 10' text='Total Créditos'/>
+                                    value={`R$ ${credit}`} text='Total de Créditos'/>
                             <ValueBox cols='12 4' color='red' icon='credit-card'
-                                    value='R$ 10' text='Total Débitos'/>
+                                    value={`R$ ${debt}`} text='Total de Débitos'/>
                             <ValueBox cols='12 4' color='blue' icon='money'
-                                    value='R$ 0' text='Total Consolidado'/>
+                                    value={`R$ ${credit - debt}`} text='Total Consolidado'/>
                         </Row>
                      </Content>
                 </div>
         )
     }
 }
-
-export default Dashboard
+const mapStateToProps = (state) => ({summary: state.dashboard.summary})
+const mapDispatchToProps = (dispatch) => bindActionCreators({getSummary}, dispatch)
+export default connect(mapStateToProps,mapDispatchToProps)(Dashboard)
