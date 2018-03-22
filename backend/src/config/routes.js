fbee905328859aad15cd8bdeff13a788a022f5e5
Luis Rabock <luis.rabock@hotmail.com>
2018-03-22 02:06:47 -0300

    Finishing backend authentication with jwt and bcrypt

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,11 +1,29 @@
 const express = require('express')
+const auth = require('./auth')
 
 
 
 module.exports = function(server) {
-    const router = express.Router()
-    server.use('/api', router)
+
+    /*rotas protegidas peo jwt*/
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router,'/billingCycles')
+    BillingCycle.register(protectedApi,'/billingCycles')
+
+
+    /*rotas abertas*/
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/authService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
+
 }
+
