4752c84c868e277f305bb17a46962ea9147ced65
Luis Rabock <luis.rabock@hotmail.com>
2018-03-27 00:32:35 -0300

    Completed authentication with jwt and bcrypt

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import '../common/template/dependencias'
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
